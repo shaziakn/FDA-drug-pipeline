8be52b6184daf52580a830873149eaf950c8e83e
Shazia Nooruddin <shaziakn@Shazias-MacBook-Pro.local>
2021-05-10 20:56:09 -0500

    incremental comment added

--- models/hospital_general_info.sql
+++ models/hospital_general_info.sql
@@ -1,3 +1,4 @@
-{{ config(materialized='table') }}
 /*{{ config(materialized='incremental') }}*/
+{{ config(materialized='table') }}
+
 SELECT * FROM {{ source("cms_medicare", "hospital_general_info") }}
