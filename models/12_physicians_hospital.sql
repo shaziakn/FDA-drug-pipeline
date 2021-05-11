7367e210a1dc907619685ddfcf7d0a4c8b163557
Shazia Nooruddin <shaziakn@Shazias-MacBook-Pro.local>
2021-05-11 00:54:21 -0500

    Added tests

--- models/12_physicians_hospital.sql
+++ models/12_physicians_hospital.sql
@@ -1,5 +1,7 @@
-SELECT hospitals.*, {{ dbt_utils.star(from=ref("model9"), except=['city', 'state']) }}
+SELECT hospitals.*, {{ dbt_utils.star(from=ref("10_clean_physicians_all"), except=['city', 'state']) }}
 FROM {{ ref("10_clean_physicians_all") }} providers
 LEFT OUTER JOIN {{ ref("11_clean_hospital_general_info") }} hospitals
 ON SUBSTR(providers.zip, 1, 5) = hospitals.zip_code
+AND providers.city = hospitals.city
+AND REGEXP_SUBSTR(providers.abbr_street, r"\S+", 1, 1) = REGEXP_SUBSTR(hospitals.abbr_address, r"\S+", 1, 1)
 AND REGEXP_SUBSTR(providers.abbr_street, r"\S+", 1, 2) = REGEXP_SUBSTR(hospitals.abbr_address, r"\S+", 1, 2)
